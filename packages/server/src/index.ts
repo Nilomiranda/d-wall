9c1eef20660d77915a2ce6c7772d373a772d08db
Nilomiranda <nilomiranda3@gmail.com>
2021-10-06 18:36:43 -0300

    first commit

---
+++ packages/server/src/index.ts
@@ -0,0 +1,30 @@
+import Koa from 'koa'
+import dotenv from 'dotenv'
+import cors from '@koa/cors'
+import router from './routes'
+import bodyParser from 'koa-bodyparser'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = new Koa()
+
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+  console.log(`Application running on port ${process.env.PORT}`)
+})
+
+prisma?.$connect().then(() => {
+  console.log("Successfully connected to prisma client")
+}).catch(err => {
+  console.error("Error trying to connect to prisma client", err)
+})
+
+app.context.prisma = prisma
+
+app.use(cors({
+  allowMethods: ['OPTIONS', 'GET', 'HEAD', 'POST', 'DELETE', 'PATCH'],
+}))
+
+app.use(bodyParser())
+app.use(router.routes())
