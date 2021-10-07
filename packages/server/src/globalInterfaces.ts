f582df0814c67ad71db2c3e955e72bbb48505d15
Nilomiranda <nilomiranda3@gmail.com>
2021-10-06 22:22:35 -0300

    refactored type name

--- packages/server/src/globalInterfaces.ts
+++ packages/server/src/globalInterfaces.ts
@@ -2,6 +2,6 @@ import type {Prisma, PrismaClient } from '@prisma/client'
 import {ParameterizedContext} from "koa";
 import Router from "koa-router";
 
-export interface KoaContext extends ParameterizedContext<any, Router.IRouterParamContext<any, {}>, any> {
+export interface ApplicationContext extends ParameterizedContext<any, Router.IRouterParamContext<any, {}>, any> {
   prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation>
 }
