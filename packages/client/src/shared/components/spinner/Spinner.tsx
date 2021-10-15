11600ffc2182dab44ebdf54015cd2cc3860a3e42
Nilomiranda <nilomiranda3@gmail.com>
2021-10-15 19:37:55 -0300

    created custom spinner component

---
+++ packages/client/src/shared/components/spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import './styles.scss'
+import loadingIconUrl from '../../../assets/icons/loading_icon.svg'
+
+interface SpinnerProps {
+  text?: string
+}
+
+const Spinner = ({ text }: SpinnerProps) => {
+  return (
+    <div className="loading-container">
+      <img className="loading-container__spinner" src={loadingIconUrl} alt="Loading spinner" />
+      { text ? <span className="loading-container__text">{text}</span> : null }
+    </div>
+  )
+}
+
+export default Spinner
