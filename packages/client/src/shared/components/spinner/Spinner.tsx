6ad1e99cf9ea19f091ead00da751a8c3a6e1a007
Nilomiranda <nilomiranda3@gmail.com>
2021-10-15 19:40:58 -0300

    using global animation class

--- packages/client/src/shared/components/spinner/Spinner.tsx
+++ packages/client/src/shared/components/spinner/Spinner.tsx
@@ -8,7 +8,7 @@ interface SpinnerProps {
 const Spinner = ({ text }: SpinnerProps) => {
   return (
     <div className="loading-container">
-      <img className="loading-container__spinner" src={loadingIconUrl} alt="Loading spinner" />
+      <img className="loading-container__spinner animate__rotate" src={loadingIconUrl} alt="Loading spinner" />
       { text ? <span className="loading-container__text">{text}</span> : null }
     </div>
   )
