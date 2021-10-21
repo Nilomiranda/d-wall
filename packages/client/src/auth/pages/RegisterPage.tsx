35db783a52a7059a06afeecf7fb69e9ad015e619
Nilomiranda <nilomiranda3@gmail.com>
2021-10-21 20:26:08 -0300

    added more registration and fixed navigation to login after account created

--- packages/client/src/auth/pages/RegisterPage.tsx
+++ packages/client/src/auth/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import {useHistory} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import {FormEvent, useState} from "react";
 import {gql, useMutation} from "@apollo/client";
 import Input from "../../shared/components/input/Input";
@@ -34,7 +34,7 @@ const RegisterPage = () => {
       }
     })
 
-    history.push('/wall')
+    history.push(`/login?email=${email}&new=true`)
   }
 
   return (
@@ -43,20 +43,22 @@ const RegisterPage = () => {
         <h1>Create your account</h1>
 
         <div className="login-page__form__input">
-          <Input placeholder="Ex.: name@domain.com" label="Email" type="email" value={email} onChange={({ target: { value } }) => setEmail(value)} />
+          <Input required placeholder="Ex.: name@domain.com" label="Email" type="email" value={email} onChange={({ target: { value } }) => setEmail(value)} />
         </div>
 
         <div className="login-page__form__input">
-          <Input placeholder="A nice name" label="Name" type="text" value={name} onChange={({ target: { value } }) => setName(value)} />
+          <Input required placeholder="A nice name" label="Name" type="text" value={name} onChange={({ target: { value } }) => setName(value)} />
         </div>
 
         <div className="login-page__form__input">
-          <Input placeholder="A safe password" label="Name" type="password" value={password} onChange={({ target: { value } }) => setPassword(value)} />
+          <Input required placeholder="A safe password" label="Name" type="password" value={password} onChange={({ target: { value } }) => setPassword(value)} />
         </div>
 
         <div className="login-page__form__footer">
           <Button type="submit" disabled={!email || !name || !password} loading={submitting} loadingText="Creating account">Create account</Button>
         </div>
+
+        <p>Already have an account? <Link to="/login">Sign in then.</Link></p>
       </form>
     </main>
   )
