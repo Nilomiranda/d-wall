7dc1fbda711ebc36e23c7f1c0ab77fcec36c2411
Nilomiranda <nilomiranda3@gmail.com>
2021-10-21 20:19:27 -0300

    created register page

--- packages/client/src/auth/pages/RegisterPage.tsx
+++ packages/client/src/auth/pages/RegisterPage.tsx
@@ -1,5 +1,65 @@
+import {useHistory} from "react-router-dom";
+import {FormEvent, useState} from "react";
+import {gql, useMutation} from "@apollo/client";
+import Input from "../../shared/components/input/Input";
+import Button from "../../shared/components/button/Button";
+import './styles/register.scss'
+
+const CREATE_NEW_ACCOUNT = gql`
+    mutation newAccount($email: String!, $name: String!, $password: String!) {
+        createUser(email: $email, password: $password, name: $name) {
+            id
+            name
+            email
+        }
+    }
+`
+
 const RegisterPage = () => {
-  return <h1>Register Page</h1>
+  const history = useHistory()
+
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [createAccount, { loading: submitting, error, data }] = useMutation(CREATE_NEW_ACCOUNT)
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    await createAccount({
+      variables: {
+        email,
+        name,
+        password
+      }
+    })
+
+    history.push('/wall')
+  }
+
+  return (
+    <main className="login-page">
+      <form onSubmit={handleSubmit} className="login-page__form">
+        <h1>Create your account</h1>
+
+        <div className="login-page__form__input">
+          <Input placeholder="Ex.: name@domain.com" label="Email" type="email" value={email} onChange={({ target: { value } }) => setEmail(value)} />
+        </div>
+
+        <div className="login-page__form__input">
+          <Input placeholder="A nice name" label="Name" type="text" value={name} onChange={({ target: { value } }) => setName(value)} />
+        </div>
+
+        <div className="login-page__form__input">
+          <Input placeholder="A safe password" label="Name" type="password" value={password} onChange={({ target: { value } }) => setPassword(value)} />
+        </div>
+
+        <div className="login-page__form__footer">
+          <Button type="submit" disabled={!email || !name || !password} loading={submitting} loadingText="Creating account">Create account</Button>
+        </div>
+      </form>
+    </main>
+  )
 }
 
 export default RegisterPage
